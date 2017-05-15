9a791189f5517cc93651a27d5b39702364eb0d16
tribta <joaocartuchoo@gmail.com>
2017-05-15 14:45:11 +0100

    python version

--- ImgProc/tutorial_threshold_inRange/threshold_in_range.py
+++ ImgProc/tutorial_threshold_inRange/threshold_in_range.py
@@ -1,6 +1,4 @@
-import sys
 import cv2
-import numpy as np
 
 low_r = 30
 low_g = 30
@@ -9,77 +7,92 @@ high_r = 100
 high_g = 100
 high_b = 100
 
+window_name = 'Object Detection'
+frame_name = 'Video Capture'
+
+
 def on_low_r_thresh_trackbar(param):
+    param = min(high_r - 1, param)
     global low_r
     low_r = param
-    #threshold_demo()
+    cv2.setTrackbarPos('Low R', window_name, low_r)
+
 
 def on_high_r_thresh_trackbar(param):
+    param = max(param, low_r + 1)
     global high_r
     high_r = param
-    #threshold_demo()
+    cv2.setTrackbarPos('High R', window_name, high_r)
+
 
 def on_low_g_thresh_trackbar(param):
+    param = min(high_g - 1, param)
     global low_g
     low_g = param
-    #threshold_demo()
+    cv2.setTrackbarPos('Low G', window_name, low_g)
+
 
 def on_high_g_thresh_trackbar(param):
+    param = max(param, low_g + 1)
     global high_g
     high_g = param
-    #threshold_demo()
+    cv2.setTrackbarPos('High G', window_name, high_g)
+
 
 def on_low_b_thresh_trackbar(param):
+    param = min(high_b - 1, param)
     global low_b
     low_b = param
-    #threshold_demo()
+    cv2.setTrackbarPos('Low B', window_name, low_b)
+
 
 def on_high_b_thresh_trackbar(param):
+    param = max(param, low_b + 1)
     global high_b
     high_b = param
-    #threshold_demo()
+    cv2.setTrackbarPos('High B', window_name, high_b)
+
 
 # [cap]
 cap = cv2.VideoCapture(0)
 # [cap]
 
 # [window]
-cv2.namedWindow("Video Capture", cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow("Object Detection", cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow(frame_name, cv2.WINDOW_AUTOSIZE)
+cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
 # [window]
 
 # [trackbar]
-#-- Trackbars to set thresholds for RGB values
-cv2.createTrackbar("Low R", "Object Detection",
+# -- Trackbars to set thresholds for RGB values
+cv2.createTrackbar('Low R', window_name,
                    low_r, 255, on_low_r_thresh_trackbar)
-cv2.createTrackbar("High R", "Object Detection",
+cv2.createTrackbar('High R', window_name,
                    high_r, 255, on_high_r_thresh_trackbar)
-cv2.createTrackbar("Low G", "Object Detection",
+cv2.createTrackbar('Low G', window_name,
                    low_g, 255, on_low_g_thresh_trackbar)
-cv2.createTrackbar("High G", "Object Detection",
+cv2.createTrackbar('High G', window_name,
                    high_g, 255, on_high_g_thresh_trackbar)
-cv2.createTrackbar("Low B", "Object Detection",
+cv2.createTrackbar('Low B', window_name,
                    low_b, 255, on_low_b_thresh_trackbar)
-cv2.createTrackbar("High B", "Object Detection",
+cv2.createTrackbar('High B', window_name,
                    high_b, 255, on_high_b_thresh_trackbar)
 # [trackbar]
 
-while(True):
+while (True):
     # [while]
     ret, frame = cap.read()
     if frame is None:
         break
 
-    #-- Detect the object based on RGB Range Values
+    # -- Detect the object based on RGB Range Values
+    frame_threshold = cv2.inRange(frame, (low_b, low_g, low_r), (high_b, high_g, high_r))
     # [while]
     # [show]
+    # -- Show the frames
+    cv2.imshow(frame_name, frame)
+    cv2.imshow(window_name, frame_threshold)
     # [show]
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
